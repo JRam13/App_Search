1e8c563a8aad18829e1140030cec411f4b7fa128
Jonny Ramos <jramos@uchicago.edu>
2013-04-30 22:42:30 -0500

    Cache done. Need to optimize in future

--- AppSearch/FetchSearchResults.m
+++ AppSearch/FetchSearchResults.m
@@ -16,6 +16,11 @@
     
     NSMutableArray *results;
     
+    NSCache *cache;
+    NSDictionary *cacheDictionary;
+    
+    NSString *replaceSpace;
+    
     
 }
 @synthesize passURL;
@@ -37,6 +42,7 @@ static FetchSearchResults *sharedInstance = nil;
     
     if (self) {
 
+        cache = [[NSCache alloc]init];
     }
     
     return self;
@@ -46,18 +52,38 @@ static FetchSearchResults *sharedInstance = nil;
 {
     
     
-    NSString *replaceSpace = [passURL stringByReplacingOccurrencesOfString:@" " withString:@"+"];
+    //check to see if data is cached
+    
+    
+    replaceSpace = [passURL stringByReplacingOccurrencesOfString:@" " withString:@"+"];
     
     NSString *currentQuery = [NSString stringWithFormat: @"https://itunes.apple.com/search?term=%@&country=us&entity=software", replaceSpace];
-    NSURL *url1 = [NSURL URLWithString:currentQuery];
-    NSURLRequest *request = [NSURLRequest requestWithURL:url1];
     
-    connection = [NSURLConnection connectionWithRequest:request delegate:self];
+    cacheDictionary = [cache objectForKey:[NSString stringWithFormat:@"%@", replaceSpace]];
+    //NSLog(@"Contents of cacheDict 1: %@", cacheDictionary);
+    
+    if(cacheDictionary == NULL){
+        NSURL *url1 = [NSURL URLWithString:currentQuery];
+        NSURLRequest *request = [NSURLRequest requestWithURL:url1];
+    
+        connection = [NSURLConnection connectionWithRequest:request delegate:self];
  
     
-    if(connection){
-        // NSLog(@"Good Connection");
-        webData = [[NSMutableData alloc] init];
+        if(connection){
+            // NSLog(@"Good Connection");
+            webData = [[NSMutableData alloc] init];
+            _arrayOfTitles = [[NSMutableArray alloc] init];
+            _arrayOfImages = [[NSMutableArray alloc] init];
+            _arrayOfAuthors = [[NSMutableArray alloc] init];
+            _arrayOfPrices = [[NSMutableArray alloc] init];
+            _arrayOfRatings = [[NSMutableArray alloc] init];
+            _arrayOfRatingsInt = [[NSMutableArray alloc] init];
+            _arrayOfDescriptions = [[NSMutableArray alloc]init];
+            _arrayOfBuyLinks = [[NSMutableArray alloc]init];
+        
+        }
+    }
+    else{
         _arrayOfTitles = [[NSMutableArray alloc] init];
         _arrayOfImages = [[NSMutableArray alloc] init];
         _arrayOfAuthors = [[NSMutableArray alloc] init];
@@ -67,6 +93,7 @@ static FetchSearchResults *sharedInstance = nil;
         _arrayOfDescriptions = [[NSMutableArray alloc]init];
         _arrayOfBuyLinks = [[NSMutableArray alloc]init];
         
+        [self fetchCacheResults];
     }
     
 }
@@ -95,8 +122,11 @@ static FetchSearchResults *sharedInstance = nil;
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         //Fetch Search Results
         NSDictionary *allSearchDataDictionary = [NSJSONSerialization JSONObjectWithData:webData options:0 error:nil];
+        cacheDictionary = allSearchDataDictionary;
+        [cache setObject:cacheDictionary forKey:replaceSpace];
+        NSLog(@"Contents of cacheDict 2: %@", [cache objectForKey:[NSString stringWithFormat:@"%@", replaceSpace]]);
         //NSLog(@"allSearchDict OK:");
-        results = [allSearchDataDictionary objectForKey:@"results"];
+        //results = [allSearchDataDictionary objectForKey:@"results"];
         //NSLog(@"results OK:");
         //NSArray *arrayOfEntry = [feed objectForKey:@"entry"];
         
@@ -188,6 +218,106 @@ static FetchSearchResults *sharedInstance = nil;
         }
     });
     
+}
+
+- (void)fetchCacheResults{
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        //Fetch Search Results
+        [cache setObject:cacheDictionary forKey:replaceSpace];
+        NSLog(@"Contents of cacheDict 2: %@", [cache objectForKey:[NSString stringWithFormat:@"%@", replaceSpace]]);
+        //NSLog(@"allSearchDict OK:");
+        //results = [allSearchDataDictionary objectForKey:@"results"];
+        //NSLog(@"results OK:");
+        //NSArray *arrayOfEntry = [feed objectForKey:@"entry"];
+        
+        
+        NSMutableDictionary *mutableResults = [NSMutableDictionary dictionaryWithDictionary:cacheDictionary];
+        
+        NSMutableArray *resultsArray;
+        
+        /*
+         
+         FOLLOWING CODE HELP BY ALEX SILVA
+         --------------------------------------------------------------
+         */
+        
+        if(_sortByRating){
+            //sort results by rating
+            resultsArray = [mutableResults[@"results"] mutableCopy];
+            [resultsArray sortUsingComparator:^(NSDictionary* dict1, NSDictionary* dict2) {
+                
+                
+                NSNumber *rating1 = [NSNumber numberWithInt:[[dict1 objectForKey:@"averageUserRating"] intValue]];
+                NSNumber *rating2 = [NSNumber numberWithInt:[[dict2 objectForKey:@"averageUserRating"] intValue]];
+                
+                return [rating2 compare: rating1];
+                
+            }];
+        }
+        
+        /*
+         
+         ---------------------------------------------------------------
+         
+         */
+        
+        if(_sortByAuthor){
+            //sort by seller name
+            resultsArray = [mutableResults[@"results"] mutableCopy];
+            [resultsArray sortUsingComparator:^(NSDictionary* dict1, NSDictionary* dict2) {
+                
+                
+                NSString *rating1 = [dict1 objectForKey:@"sellerName"] ;
+                NSString *rating2 = [dict2 objectForKey:@"sellerName"] ;
+                
+                return [rating1 compare: rating2];
+                
+            }];
+            
+        }
+        
+        //NSLog(@"last object: %@", resultsArray);
+        for (NSDictionary *diction in resultsArray) {
+            //NSLog(@"Diction: %@", diction);
+            //NSLog(@"8");
+            NSString *label = [diction objectForKey:@"trackName"];
+            //NSLog(@"9");
+            NSString *image = [diction objectForKey:@"artworkUrl60"];
+            NSData *imageData = [[NSData alloc] initWithContentsOfURL: [NSURL URLWithString: image]];
+            NSObject *imageObject = [UIImage imageWithData: imageData];
+            
+            NSString *author = [diction objectForKey:@"sellerName"];
+            
+            NSString *price = [diction objectForKey:@"formattedPrice"];
+            
+            NSUInteger rating = [[diction objectForKey:@"averageUserRating"] integerValue];
+            UIImage *ratingsImage = [UIImage imageNamed:[NSString stringWithFormat:@"%dstars.png", rating]];
+            NSString *ratingString = [NSString stringWithFormat:@"%d", rating];
+            //NSLog(@"Number String: arrayLast %@", ratingString);
+            
+            NSString *desc = [diction objectForKey:@"description"];
+            
+            NSString *buy = [diction objectForKey:@"trackViewUrl"];
+            
+            
+            // dispatch_sync(dispatch_get_main_queue(), ^{
+            [_arrayOfTitles addObject:label];
+            //NSLog(@"Rating: %d", rating);
+            [_arrayOfImages addObject:imageObject];
+            [_arrayOfAuthors addObject:author];
+            [_arrayOfPrices addObject:price];
+            [_arrayOfRatings addObject:ratingsImage];
+            [_arrayOfRatingsInt addObject:ratingString];
+            [_arrayOfDescriptions addObject:desc];
+            [_arrayOfBuyLinks addObject:buy];
+            //NSLog(@"Description from fetch: %@", desc);
+            //});
+            //NSLog(@"Title: %@", label);
+            //NSLog(@"Number: arrayLast %@", _arrayOfRatingsInt.lastObject);
+            // NSLog(@"Image: arrayLast %@", _arrayOfImages.lastObject);
+        }
+    });
 }
 
 
